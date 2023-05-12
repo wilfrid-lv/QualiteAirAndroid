38ac7d01c77448d88ff0def02e4b69eaac1821ca
wilfrid-lv <wilfrid-levalegant@live.fr>
2023-05-12 14:31:12 -0400

    2ème commit, un changement de langue a été réussi

--- app/src/main/java/com/example/recuperevue/MainActivity.kt
+++ app/src/main/java/com/example/recuperevue/MainActivity.kt
@@ -7,29 +7,106 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.util.Log
-import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
 import android.widget.Toast
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-
-
+import android.widget.AdapterView
+import android.content.res.Configuration
+import androidx.appcompat.app.AlertDialog
+import android.app.Activity
+import android.content.Context
+import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
 
+    //lateinit var locale: Locale
+    lateinit var mBtn :Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+        setLocate("fr")
+        //loadLocate()
+
+
+
+        val btnConnexion = findViewById<Button>(R.id.connexion)
+        btnConnexion.setOnClickListener {
+            val intent = Intent(this, Qualite::class.java)
+            //Log.d("MainActivity", "Bouton de connexion appuyé")
+            startActivity(intent)
+        }
+        val actionBar = supportActionBar
+        actionBar!!.title = resources.getString(R.string.app_name)
+
+
+        mBtn = findViewById(R.id.langue)
+
+        mBtn.setOnClickListener {
+
+            showChangeLang()
+
+        }
+    }
+
+    private fun showChangeLang() {
+
+        val listItmes = arrayOf("Français", "English", "Español")
+
+        val mBuilder = AlertDialog.Builder(this@MainActivity)
+        mBuilder.setTitle("Choose Language")
+        mBuilder.setSingleChoiceItems(listItmes, -1) { dialog, which ->
+            if (which == 0) {
+                setLocate("fr")
+                recreate()
+            } else if (which == 1) {
+                setLocate("en")
+                recreate()
+            } else if (which == 2) {
+                setLocate("es")
+                recreate()
+            }
+
+            dialog.dismiss()
+        }
+        val mDialog = mBuilder.create()
+
+        mDialog.show()
+
+    }
+
+    private fun setLocate(Lang: String) {
+        val locale = Locale(Lang)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        val newContext = createConfigurationContext(config)
+        //baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", Lang)
+        editor.apply()
+    }
+
+    private fun loadLocate() {
+        val sharedPreferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        val language = sharedPreferences.getString("My_Lang", null)
+        if (language != null) {
+            setLocate(language)
+        }
+    }
 
     fun connexion(view: View) {
         val titre = findViewById<TextView>(R.id.Titre)
         val identifiant = findViewById<EditText>(R.id.identifiant)
         val mdp = findViewById<EditText>(R.id.MotDePasse)
         val connexion = findViewById<Button>(R.id.connexion)
-        connexion.setOnClickListener {
+        /*connexion.setOnClickListener {
             val intent = Intent(this, Qualite::class.java)
             startActivity(intent)
-        }
+        }*/
 
         if (identifiant.text.toString() == "wilfrid" && mdp.text.toString() == "Patate123"){
             val intent = Intent(this, Qualite::class.java)
@@ -40,10 +117,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {//il faudrait mettre connexion dedans et enable le bouton tant que des infos ne sont pas renté dans les champs
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       /*private fun setLocale (languageName : String){
+            locale = Locale(languageName)
+            val res = resources
+            val dm = res.displayMetrics
+            val conf = res.configuration
+            val localeList = ArrayList<Locale>()
+            localeList.add(locale)
+            conf.setLocale(locale)
+            res.updateConfiguration(conf, dm)
 
+           /* var refresh = Intent(ActivityMainBinding@this,ActivityMainBinding::class.java)
+            startActivity(intent)*/
+           val refresh = Intent(this, MainActivity::class.java)
+           startActivity(refresh)
+           finish()
+        }*/
+
+        /*
         //initialiser le choix de langue
         val languages = resources.getStringArray(R.array.choix_langues).toList()
         val spinner = findViewById<Spinner>(R.id.spinner_langues)
@@ -51,11 +142,24 @@ class MainActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
-        /*
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 val selectedLanguage = languages[position]
-                // TODO: Changer la langue de l'application
+                //if(spinner.selectedLanguage.tooString == 'Français')
+                val selectedLanguage = languages[position]
+
+                if(selectedLanguage == "Français") {
+                    // Charger les ressources pour la langue française
+                    val config = Configuration(resources.configuration)
+                    config.setLocale(Locale.FRENCH)
+                    resources.updateConfiguration(config, resources.displayMetrics)
+                }
+                else {
+                    // Charger les ressources pour la langue par défaut (anglais)
+                    val config = Configuration(resources.configuration)
+                    config.setLocale(Locale.getDefault())
+                    resources.updateConfiguration(config, resources.displayMetrics)
+                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -64,15 +168,6 @@ class MainActivity : AppCompatActivity() {
         }*/
 
 
-
-
-
-        val btnConnexion = findViewById<Button>(R.id.connexion)
-        btnConnexion.setOnClickListener {
-            val intent = Intent(this, Qualite::class.java)
-            Log.d("MainActivity", "Bouton de connexion appuyé")
-            startActivity(intent)
-        }
 
 
 
@@ -143,7 +238,3 @@ class MainActivity : AppCompatActivity() {
         */
 
     }
-
-
-
-}
---
+++ app/src/main/java/com/example/recuperevue/MyContextWrapper.kt
@@ -0,0 +1,57 @@
+package com.example.recuperevue
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Configuration
+import android.os.Build
+import java.util.Locale
+
+class MyContextWrapper(base: Context) : ContextWrapper(base) {
+
+    companion object {
+        fun wrap(context: Context, language: String): Context? {
+            val config = context.resources.configuration
+            val sysLocale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                getSystemLocale(config)
+            } else {
+                getSystemLocaleLegacy(config)
+            }
+            if (!language.isEmpty() && sysLocale.language != language) {
+                val locale = Locale(language)
+                Locale.setDefault(locale)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    setSystemLocale(config, locale)
+                } else {
+                    setSystemLocaleLegacy(config, locale)
+                }
+            }
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                context.createConfigurationContext(config)
+            } else {
+                context.resources.updateConfiguration(config, context.resources.displayMetrics)
+                MyContextWrapper(context)
+            }
+        }
+
+        @Suppress("DEPRECATION")
+        private fun getSystemLocaleLegacy(config: Configuration): Locale {
+            return config.locale
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        private fun getSystemLocale(config: Configuration): Locale {
+            return config.locales.get(0)
+        }
+
+        @Suppress("DEPRECATION")
+        private fun setSystemLocaleLegacy(config: Configuration, locale: Locale) {
+            config.locale = locale
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        private fun setSystemLocale(config: Configuration, locale: Locale) {
+            config.setLocale(locale)
+        }
+    }
+}
